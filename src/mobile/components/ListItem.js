e952dac13573f5ce8de38ec4a24a2547be9c0548
Uttej Guduru <uttejg@angieslist.com>
2016-04-30 15:31:42 -0400

    Add activity functionality

---
+++ src/mobile/components/ListItem.js
@@ -0,0 +1,44 @@
+var React = require('react-native');
+const { View, TouchableHighlight, Text } = React;
+
+class ListItem extends React.Component {
+    render() {
+        return (
+            <TouchableHighlight onPress={this.props.onPress}>
+                <View style={styles.li}>
+                    <Text style={styles.date}>
+                        {this.props.item.date}
+                    </Text>
+                    <Text style={styles.liText}>
+                        <Text style={styles.bold}>{this.props.item.user}</Text> said "the park was {this.props.item.crowd}. The condition of the equipment in the park is {this.props.item.equipmentCondition} and my overall experience is {this.props.item.overallExperience}"
+                    </Text>
+                </View>
+            </TouchableHighlight>
+        );
+    }
+}
+
+const styles = React.StyleSheet.create({
+    li: {
+        backgroundColor: '#fff',
+        borderBottomColor: '#eee',
+        borderColor: 'transparent',
+        borderWidth: 1,
+        paddingLeft: 16,
+        paddingTop: 14,
+        paddingBottom: 16,
+    },
+    liText: {
+        color: '#333',
+        fontSize: 16,
+    },
+    date: {
+        color: '#A9A9A9',
+        fontStyle: 'italic'
+    },
+    bold: {
+        fontWeight: 'bold'
+    }
+});
+
+module.exports = ListItem;
--- src/mobile/views/parkActivity.js
+++ src/mobile/views/parkActivity.js
@@ -2,34 +2,71 @@ import React from 'react-native';
 const {
   Component,
   StyleSheet,
+  ListView,
   Text,
   TouchableHighlight,
   View
 } = React;
+const ListItem = require('../components/ListItem');
+const Firebase = require('firebase');
 
 class report extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = new Firebase("indypark.firebaseio.com/reports");
+  }
+
+  _renderItem(item) {
+    return (
+        <ListItem item={item} onPress={() => {}} />
+    );
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.orderByChild("date").on('value', (snap) => {
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          user: child.val().user,
+          crowd: child.val().crowd,
+          equipmentCondition: child.val().equipmentCondition,
+          overallExperience: child.val().overallExperience,
+          date: child.val().date,
+          _key: child.key()
+        });
+      });
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Activity
-        </Text>
-      </View>
+        <View style={styles.container}>
+          <ListView
+              dataSource={this.state.dataSource}
+              renderRow={this._renderItem.bind(this)}
+          />
+        </View>
     );
   }
 }
 
 const styles = React.StyleSheet.create({
   container: {
+    paddingTop:65,
+    backgroundColor: '#f2f2f2',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
   }
 });
 
